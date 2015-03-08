2a6a0df9fedbb2f2ec9a751971e8641bbe478ee4
Christopher Liscio <chris@supermegaultragroovy.com>
2015-03-08 16:58:45 -0400

    Initial Commit

---
+++ FunctionalDSP/FunctionalDSP.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalDSP.h
+//  FunctionalDSP
+//
+//  Created by Christopher Liscio on 3/8/15.
+//  Copyright (c) 2015 SuperMegaUltraGroovy, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FunctionalDSP.
+FOUNDATION_EXPORT double FunctionalDSPVersionNumber;
+
+//! Project version string for FunctionalDSP.
+FOUNDATION_EXPORT const unsigned char FunctionalDSPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalDSP/PublicHeader.h>
+
+
